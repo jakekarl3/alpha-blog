c2605e425f0bd6389f47294fdd282392fa737084
Jake Karl <jakekarl3@gmail.com>
2018-11-01 04:29:08 +0000

    Fixing error in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-config.assets.initialize_on_precompile = false
-
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -10,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module AlphaBlog
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
